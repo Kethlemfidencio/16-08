d30738675b8b8b9cc5cd8de159b699bf9a519099
Keth Lindonaa <126172021+Kethlemfidencio@users.noreply.github.com>
2024-08-16 09:00:24 -0300

    Add files via upload

---
+++ keth e maira 16-08/main.js
@@ -0,0 +1,50 @@
+const numeroSenha = document. querySelector('.parametro-senha_texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const botoes = document.querySelectorAll('.parametro-senha_botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+        // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+        }
+        numeroSenha.textContent = tamanhoSenha;
+        geraSenha();
+}
+function aumentaTamanho(){
+    if(tamanhoSenha < 20){
+        // tamanhoSenha = tamanhoSenha+1;
+        tamanhoSenha++;
+        }
+        numeroSenha.textContent = tamanhoSenha;
+        geraSenha();
+}
+
+for (i = 0; i < checkbox.length; i++) {
+    checkbox[i].onclick = geraSenha;
+}
+
+geraSenha();
+
+function geraSenha() {
+    let alfabeto = '';
+    if (checkbox[0].checked) {
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked) {
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked) {
+        alfabeto = alfabeto + numeros;
+    }
+}
